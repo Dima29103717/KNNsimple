1fc2b27319b534342adf02ad80490b83cc478259
Dima29103717 <limondimon261@gmail.com>
2022-08-21 18:18:59 +0300

    Обьявили основыне типы и операции

--- KNNnaive.h
+++ KNNnaive.h
@@ -1,5 +1,17 @@
 #pragma once
+#include "vecoperation.h"
+
 class KNNnaive
 {
+	private:
+		vecop::features *__X;
+		vecop::class_label *__Y;
+		int __k;
+
+	public:
+		KNNnaive(int k = 3);
+		
+		void fit(const vecop::features &X,const vecop::class_label &Y);
+		int predict(const vecop::feature &X);
 };
 
---
+++ vecoperation.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <vector>
+
+namespace vecop 
+{
+	typedef	std::vector<double> feature;
+	typedef std::vector<std::vector<double>> features;
+	typedef std::vector<int> class_label;
+}
